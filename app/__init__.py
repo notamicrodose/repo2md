334c91f53be87282af179985966f659eb8a3dc1f
notamicrodose <gabrielkripalani@me.com>
2024-08-26 00:27:03 +0200

    resolved content-security-policy issue

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ def create_app(config_class=Config):
     def add_csp_header(response):
         csp = (
             "default-src 'self'; "
-            "script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; "
+            "script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net; "  # Added https://cdn.jsdelivr.net
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com; "
             "font-src 'self' https://fonts.gstatic.com; "
             "img-src 'self' data:;"
